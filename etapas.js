331463e0ad8307a7aac80791c43a909dcdc5d6d6
BrunaVeras <85624987+BrunaVeras@users.noreply.github.com>
2021-10-26 09:58:17 -0300

    adicionando js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: 'Elfo',
+        partido: 'ENCANTO',
+        fotos: [
+          {url:'Elfo'}
+        ]
+      },
+      {
+        numero: 12345,
+        nome: 'Beltrano',
+        partido: 'DESENCANTO',
+        fotos: [
+          {url}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: 'Ciclano',
+        partido: 'ABC',
+        vice: 'cic',
+        fotos:[
+          {},
+          {}
+        ]
+      },
+      {
+        numero: 84,
+        nome: 'Fulano',
+        partido: 'REACT',
+        vice: 'NATIVE',
+        fotos:[
+          {},
+          {}
+        ]
+      },
+    ]
+  }
+];
